bed918082517a66b26d83605f52aab9eff416387
Ivan Burmistrov <vanya.burmistrov007@gmail.com>
2022-11-29 15:21:02 +0400

    4 tasks of the third block

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -5,4 +5,20 @@
  */
 export function createGetter(path) {
 
+    const arrOfParams = path.split('.').reverse();
+    let sizeOfParams = arrOfParams.length;
+
+    return function foo(obj) {
+        if (Object.keys(obj).length === 0) {
+            return
+        }
+
+        if (sizeOfParams === 1) {
+            const target = arrOfParams[0]
+            return obj[target]
+        }
+
+        const prevParam = arrOfParams[--sizeOfParams]
+        return foo(obj[prevParam])
+    }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (size === 0) return '';
+    if (!size) return string;
 
+    const arrFromString = string.split('')
+    const result = [];
+
+    result.push(arrFromString[0])
+    let numOfRepeat = 1;
+
+    for (let i = 1; i < arrFromString.length; i++) {
+        const currentItem = arrFromString[i];
+        const prevItem = arrFromString[i - 1];
+
+        const same = (currentItem === prevItem);
+
+        if (same && numOfRepeat < size) {
+            result.push(currentItem);
+            numOfRepeat++
+        }
+
+        if (!same) {
+            result.push(currentItem);
+            numOfRepeat = 1
+        }
+    }
+    return result.join('')
 }
