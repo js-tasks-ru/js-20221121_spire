8485f264cf615bd58373c51ce489fac916216255
Ivan Burmistrov <vanya.burmistrov007@gmail.com>
2022-12-18 19:23:08 +0400

    add change/new behave

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -1,15 +1,203 @@
-import escapeHtml from './utils/escape-html.js';
-import fetchJson from './utils/fetch-json.js';
+import escapeHtml from "./utils/escape-html.js";
+import fetchJson from "./utils/fetch-json.js";
 
-const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
-const BACKEND_URL = 'https://course-js.javascript.ru';
+const IMGUR_CLIENT_ID = "28aaa2e823b03b1";
+const BACKEND_URL = "https://course-js.javascript.ru";
 
 export default class ProductForm {
-  constructor (productId) {
+  product = null;
+
+  constructor(productId) {
     this.productId = productId;
+    // this.render();
   }
 
-  async render () {
+  async render() {
+    if (this.productId) {
+      await this.getProduct();
+    }
+    await this.getCategories();
+    const root = document.getElementById("root");
+    const div = document.createElement("div");
+    div.innerHTML = this.productForm();
+    this.element = div;
 
+    root.append(this.element);
+
+    if (this.product) {
+      this.update();
+    }
   }
+
+  async getProduct() {
+    const respond = await fetch(
+      `${BACKEND_URL}/api/rest/products?id=${this.productId}`
+    );
+    const result = await respond.json();
+    this.product = result[0];
+  }
+
+  async getCategories() {
+    const respond = await fetch(
+      `${BACKEND_URL}/api/rest/categories?_sort=weight&_refs=subcategory`
+    );
+    this.categories = await respond.json();
+  }
+
+  // id перадан?
+  // + вызвать
+  // - не вызывать
+  update() {
+    const {
+      title,
+      description,
+      price,
+      discount,
+      quantity,
+      status,
+      subcategory,
+    } = this.product;
+
+    const mainForm = document.forms[0];
+    mainForm.title.value = title;
+    mainForm.description.value = description;
+    mainForm.price.value = price;
+    mainForm.discount.value = discount;
+    mainForm.quantity.value = quantity;
+    mainForm.status.value = status;
+    mainForm.subcategory.value = subcategory;
+    mainForm.save.innerHTML = "Сохранить товар";
+  }
+
+  productForm() {
+    return `<div class="product-form">
+              <form data-element="productForm" class="form-grid">
+                ${this.productTitle()}
+                ${this.productDiscription()}
+                ${this.productPhoto()}
+                ${this.productCategory()}
+                ${this.productValue()}
+                ${this.productQuantity()}
+                ${this.productStatus()}
+                ${this.productSaveButton()}
+              </form>
+            </div>`;
+  }
+
+  productTitle() {
+    return `<div class="form-group form-group__half_left">
+              <fieldset>
+                <label class="form-label">Название товара</label>
+                <input required="" type="text" name="title" class="form-control" placeholder="Название товара">
+              </fieldset>
+            </div>`;
+  }
+
+  productDiscription() {
+    return `<div class="form-group form-group__wide">
+              <label class="form-label">Описание</label>
+              <textarea required="" class="form-control" name="description" data-element="productDescription" placeholder="Описание товара"></textarea>
+            </div>`;
+  }
+
+  imageContainer(image) {
+    const { url, source } = image;
+
+    return `<div data-element="imageListContainer">
+              <ul class="sortable-list">
+              <li class="products-edit__imagelist-item sortable-list__item" style="">
+                  <input type="hidden" name="url" value="${url}"/>
+                  <input type="hidden" name="source" value="${source}"/>
+                  <span>
+                    <img src="icon-grab.svg" data-grab-handle="" alt="grab" />
+                    <img class="sortable-table__cell-img" alt="Image" src="${url}"/>
+                    <span>${source}</span>
+                  </span>
+                  <button type="button">
+                    <img src="icon-trash.svg" data-delete-handle="" alt="delete"/>
+                  </button>
+                </li>
+              </ul>
+            </div>`;
+  }
+
+  productPhoto() {
+    // id передан?
+    // + создать imageContainer
+    // - вернуть пустую строку
+    return `<div class="form-group form-group__wide" data-element="sortable-list-container">
+              <label class="form-label">Фото</label>
+              ${
+                this.product
+                  ? this.product.images
+                      .map((image) => {
+                        return this.imageContainer(image);
+                      })
+                      .join("")
+                  : ""
+              }
+              <button type="button" name="uploadImage" class="button-primary-outline"><span>Загрузить</span></button>
+            </div>`;
+  }
+
+  productCategoryOption(category, categoryTitle) {
+    const { subcategories } = category;
+    return subcategories.map((sub) => {
+      return `<option value="${sub.id}">${categoryTitle} &gt; ${sub.title}</option>`;
+    });
+  }
+
+  // TODO: Разобраться с этим
+  productCategory() {
+    return `<div class="form-group form-group__half_left">
+    <label class="form-label">Категория</label>
+    <select class="form-control" name="subcategory">
+      ${this.categories.map((category) => {
+        return this.productCategoryOption(category, category.title);
+      })}
+    </select>
+  </div>`;
+  }
+
+  productValue() {
+    return `<div class="form-group form-group__half_left form-group__two-col">
+              <fieldset>
+                <label class="form-label">Цена ($)</label>
+                <input required="" type="number" name="price" class="form-control" placeholder="100">
+              </fieldset>
+              <fieldset>
+                <label class="form-label">Скидка ($)</label>
+                <input required="" type="number" name="discount" class="form-control" placeholder="0">
+              </fieldset>
+            </div>`;
+  }
+
+  productQuantity() {
+    return `<div class="form-group form-group__part-half">
+              <label class="form-label">Количество</label>
+              <input required="" type="number" class="form-control" name="quantity" placeholder="1">
+            </div>`;
+  }
+
+  productStatus() {
+    return `<div class="form-group form-group__part-half">
+              <label class="form-label">Статус</label>
+              <select class="form-control" name="status">
+                <option value="1">Активен</option>
+                <option value="0">Неактивен</option>
+              </select>
+            </div>`;
+  }
+
+  productSaveButton() {
+    return `<div class="form-buttons">
+              <button type="submit" name="save" class="button-primary-outline">
+                Добавить товар
+              </button>
+            </div>`;
+  }
+
+  remove() {}
+
+  destriy() {}
 }
